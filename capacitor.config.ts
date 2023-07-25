abe2b3ddf96fb47cea85fcbc0ff6bd4d9f779b17
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2023-07-25 13:33:34 -0300

    Adicionado Tailwind

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'doce-canto',
+  appId: 'com.docecanto.coral',
+  appName: 'Coral Doce Canto',
   webDir: 'www',
   server: {
     androidScheme: 'https'
